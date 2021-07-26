4ac880679dcc1a149b4ba907ec587aaac6f60582
Matthias Müller <matthias.mueller@it-sure.de>
2021-07-26 17:17:33 +0200

    Added AVD-FSLProperties

---
+++ AVD-FSLProperties/DSC-AVD-FSLProperties.ps1
@@ -0,0 +1,81 @@
+Configuration FSLProperties
+{
+    param(
+        [string[]]$VHDLocations
+    )
+    Node localhost
+    {
+        Registry FSLPropertiesReg-Enabled
+        {
+            Ensure = 'Present'
+            Key = 'Computer\HKEY_LOCAL_MACHINE\SOFTWARE\FSLogix\Profiles'
+            ValueName = 'Enabled'
+            ValueType = 'Dword'
+            ValueData = '1'
+        }
+        Registry FSLPropertiesReg-DeleteLocalProfileWhenVHDShouldApply
+        {
+            Ensure = 'Present'
+            Key = 'Computer\HKEY_LOCAL_MACHINE\SOFTWARE\FSLogix\Profiles'
+            ValueName = 'DeleteLocalProfileWhenVHDShouldApply'
+            ValueType = 'Dword'
+            ValueData = '1'
+        }
+        Registry FSLPropertiesReg-FlipFlopProfileDirectoryName
+        {
+            Ensure = 'Present'
+            Key = 'Computer\HKEY_LOCAL_MACHINE\SOFTWARE\FSLogix\Profiles'
+            ValueName = 'FlipFlopProfileDirectoryName'
+            ValueType = 'Dword'
+            ValueData = '1'
+        }
+        Registry FSLPropertiesReg-IsDynamic
+        {
+            Ensure = 'Present'
+            Key = 'Computer\HKEY_LOCAL_MACHINE\SOFTWARE\FSLogix\Profiles'
+            ValueName = 'IsDynamic'
+            ValueType = 'Dword'
+            ValueData = '1'
+        }
+        Registry FSLPropertiesReg-PreventLoginWithTempProfile
+        {
+            Ensure = 'Present'
+            Key = 'Computer\HKEY_LOCAL_MACHINE\SOFTWARE\FSLogix\Profiles'
+            ValueName = 'PreventLoginWithTempProfile'
+            ValueType = 'Dword'
+            ValueData = '1'
+        }
+        Registry FSLPropertiesReg-ProfileType
+        {
+            Ensure = 'Present'
+            Key = 'Computer\HKEY_LOCAL_MACHINE\SOFTWARE\FSLogix\Profiles'
+            ValueName = 'ProfileType'
+            ValueType = 'Dword'
+            ValueData = '0'
+        }
+        Registry FSLPropertiesReg-SizeInMBs
+        {
+            Ensure = 'Present'
+            Key = 'Computer\HKEY_LOCAL_MACHINE\SOFTWARE\FSLogix\Profiles'
+            ValueName = 'SizeInMBs'
+            ValueType = 'Dword'
+            ValueData = '100000'
+        }
+        Registry FSLPropertiesReg-VolumeType
+        {
+            Ensure = 'Present'
+            Key = 'Computer\HKEY_LOCAL_MACHINE\SOFTWARE\FSLogix\Profiles'
+            ValueName = 'VolumeType'
+            ValueType = 'String'
+            ValueData = 'VHDX'
+        }
+        Registry FSLPropertiesReg-VHDLocations
+        {
+            Ensure = 'Present'
+            Key = 'Computer\HKEY_LOCAL_MACHINE\SOFTWARE\FSLogix\Profiles'
+            ValueName = 'VHDLocations'
+            ValueType = 'MultiString'
+            ValueData = $VHDLocations
+        }
+    }
+}
