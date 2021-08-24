4a84f4b608db5a05f2f646a5124ca7879d4eb380
Matthias Müller <matthias.mueller@it-sure.de>
2021-08-24 10:58:30 +0200

    Added InstallSingleADDC

---
+++ CMCS-InstallSingleADDC/DSC-CMCS-InstallSingleADDC.ps1
@@ -0,0 +1,72 @@
+﻿Configuration InstallSingleADDC
+{
+    param
+   (
+        [Parameter(Mandatory)]
+        [String]$DomainName,
+
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$Admincreds
+
+    )
+
+	Import-DscResource -ModuleName xActiveDirectory, xNetworking, PSDesiredStateConfiguration, xPendingReboot
+    [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
+    $Interface= Get-NetAdapter | Where-Object Name -Like "Ethernet*"|Select-Object -First 1
+    $InterfaceAlias=$($Interface.Name)
+    Node localhost
+    {
+       LocalConfigurationManager{            
+            ActionAfterReboot = 'ContinueConfiguration'            
+            ConfigurationMode = 'ApplyOnly'            
+            RebootNodeIfNeeded = $true            
+        }
+        WindowsFeature DNS{
+            Ensure = "Present"
+            Name = "DNS"
+        }
+        
+        WindowsFeature DnsTools{
+            Ensure = "Present"
+            Name = "RSAT-DNS-Server"
+            DependsOn = "[WindowsFeature]DNS"
+        }
+        xDnsServerAddress DnsServerAddress
+        {
+            Address        = '127.0.0.1'
+            InterfaceAlias = $InterfaceAlias
+            AddressFamily  = 'IPv4'
+            DependsOn = "[WindowsFeature]DNS"
+        }
+        WindowsFeature RSAT{
+            Ensure = "Present"
+            Name = "RSAT"
+        }
+        WindowsFeature ADDSInstall{ 
+            Ensure = "Present" 
+            Name = "AD-Domain-Services"
+            DependsOn="[WindowsFeature]DNS"
+        }
+        WindowsFeature ADDSTools{
+            Ensure = "Present"
+            Name = "RSAT-ADDS-Tools"
+            DependsOn = "[WindowsFeature]ADDSInstall"
+        }
+        WindowsFeature ADAdminCenter{
+            Ensure = "Present"
+            Name = "RSAT-AD-AdminCenter"
+            DependsOn = "[WindowsFeature]ADDSTools"
+        }
+        xADDomain FirstDS
+        {
+            DomainName = $DomainName
+            DomainAdministratorCredential = $DomainCreds
+            SafemodeAdministratorPassword = $DomainCreds
+            DependsOn = "[WindowsFeature]ADDSInstall"
+        }
+        xPendingReboot RebootAfterPromotion{
+            Name = "RebootAfterPromotion"
+            DependsOn = "[xADDomain]FirstDS"
+        }
+    }
+}
