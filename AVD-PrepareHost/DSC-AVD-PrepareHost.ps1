94af8486aa691abb74f576bf8f49914eb7b6cf8a
Matthias Müller <matthias.mueller@it-sure.de>
2021-09-21 14:39:00 +0200

    Bugfix#10

--- AVD-PrepareHost/DSC-AVD-PrepareHost.ps1
+++ AVD-PrepareHost/DSC-AVD-PrepareHost.ps1
@@ -134,7 +134,7 @@ Configuration PrepareHost
             }
         }
         Script JoinDomain{
-            DependsOn = 'InstallAVDAdgent'
+            DependsOn = '[Script]InstallAVDAdgent'
             GetScript = {
                 GetScript = {
                     return @{'Result' = ''}
